e5080d9657dfbfc9de2593ec749e188afe01f5e1
Sonrebe98 <andrerebo98@gmail.com>
2021-10-21 10:28:21 +0200

    draft

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CountDownWidget :timer="counter.endDate"></CountDownWidget>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountDownWidget from "./components/CountDownWidget.vue";
+import axios from "axios";
+import moment from "moment";
+const BASE_URL = " http://192.168.1.108:8000";
+const COUNTDOWN_API = BASE_URL + "/get_countdown/";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CountDownWidget,
+  },
+  data() {
+    return {
+      counter: {
+        name: "",
+        startDate: "",
+        endDate: "",
+               
+      },
+    tempEndDate: Date(), 
+
+    };
+  },
+  methods: {
+    startCounter() {
+      setInterval(() => {
+        this.tempEndDate= Date(this.tempEndDate-1);
+        console.log( moment(this.tempEndDate).format('YYYY-MM-DD HH:mm:ss'));
+        this.counter.endDate = moment(this.tempEndDate).format('YYYY-MM-DD HH:mm:ss');
+      }, 1000);
+    },
+    getCounter(){
+      axios.get(COUNTDOWN_API).then((resp) => {
+        var data = resp.data;
+        console.log(data.start_date);
+        // var end_date =  moment(data.end_date, "dd-mm-YYYY HH:MM:SS");
+        this.tempEndDate = Date.parse(data.endDate);
+      });
+    }
+  },
+  mounted() {
+    this.getCounter();
+    this.startCounter();
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  height: 100vh;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/CountDownWidget.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="countDown">
+        <h1 class="counter">
+            {{timer}}
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "CountDownWidget",
+    props:{
+        timer: {
+            type: String,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .countDown{
+        .counter{
+            font-weight: 800;
+            font-size: 3rem;
+            color: #262739;
+        }
+    }
+</style>
