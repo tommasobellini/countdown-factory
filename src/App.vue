5726748f3ea290ceac4a5f10e5b0fff84dc2f997
Sonrebe98 <andrerebo98@gmail.com>
2021-10-21 23:36:44 +0200

    heroku server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <CountDownWidget :timer="counter.counterString" :state="counterState"></CountDownWidget>
+  <CountDownWidget
+    :timer="counter.counterString"
+    :state="counterState"
+  ></CountDownWidget>
 </template>
 <script>
 import CountDownWidget from "./components/CountDownWidget.vue";
-// import axios from "axios";
+import axios from "axios";
 import moment from "moment";
 moment.locale("it");
-// const BASE_URL = " http://192.168.1.108:8000";
-// const COUNTDOWN_API = BASE_URL + "/get_countdown/";
+const BASE_URL = "https://countdown-factory.herokuapp.com";
+const COUNTDOWN_API = BASE_URL + "/get_countdown/";
 
 export default {
   name: "App",
@@ -31,28 +34,22 @@ export default {
   },
   methods: {
     startCounter() {
-      var tempEndDate = Date.parse("2021-10-21 23:03:00");
-      var x = moment(tempEndDate);
-      var y = moment(Date.now());
-      var duration = moment.duration(x.diff(y));
-      console.log("temp: " + duration.days());
-      this.tempCountDownTime = duration;
       setInterval(() => {
         this.tempCountDownTime = this.tempCountDownTime.subtract(1, "s");
         this.getCounter();
         this.getImage();
       }, 1000);
     },
-    // getCounter(){
-    //   axios.get(COUNTDOWN_API).then((resp) => {
-    //     var data = resp.data;
-    //     console.log(data.start_date);
-    //     var tempEndDate = Date.parse(data.end_date);
-    //     // var tempStartDate = Date.parse(date.start_date);
-    //     var temp = moment(tempEndDate, 'YYYY-MM-DD HH:mm:ss').diff(Date.now(), 'YYYY-MM-DD HH:mm:ss');
-    //     this.tempCountDownTime = temp;
-    //   });
-    // }
+    getCounterAPI() {
+      axios.get(COUNTDOWN_API).then((resp) => {
+        var data = resp.data;
+        var tempEndDate = Date.parse(data[0].end_date);
+        var x = moment(tempEndDate);
+        var y = moment(Date.now());
+        var duration = moment.duration(x.diff(y));
+        this.tempCountDownTime = duration;
+      });
+    },
     addZero(num = "") {
       if (num.length < 2) {
         return "0" + num;
@@ -76,19 +73,23 @@ export default {
       console.log(this.counter.counterString);
     },
 
-    getImage(){
-      if(this.counter.days == 0 && this.counter.hours==0 && this.counter.minutes==0 && this.counter.seconds==0){
+    getImage() {
+      if (
+        this.counter.days == 0 &&
+        this.counter.hours == 0 &&
+        this.counter.minutes == 0 &&
+        this.counter.seconds == 0
+      ) {
         this.counterState = true;
-        console.log(this.counterState);
-      }
-      else {
+        this.getCounterAPI();
+      } else {
         this.counterState = false;
-      console.log(this.counterState);
+        
       }
-    }
+    },
   },
   mounted() {
-    // this.getCounter();
+    this.getCounterAPI();
     this.startCounter();
   },
 };
